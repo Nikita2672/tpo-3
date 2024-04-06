1970a31d6d958040569dabc67a35bc1a3a0abb9d
ruskaof <199-41@mail.ru>
2024-04-06 15:47:08 +0300

    fix searchpagetest

--- src/test/kotlin/SearchPageTest.kt
+++ src/test/kotlin/SearchPageTest.kt
@@ -1,3 +1,4 @@
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
 import org.openqa.selenium.By
@@ -16,19 +17,18 @@ class SearchPageTest : AbstractPageTest("https://timeweb.com/ru/search/") {
     @ParameterizedTest
     @MethodSource("browserProvider")
     fun `check knowledge base`(browser: String) {
-        // вот этот тест почему-то не перенаправляет на другую страницу, хотя все если вызывать: val element = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"__layout\"]/div/div[3]/div[2]/div/div[2]/div[1]/div[1]/div[1]")))
-        //        element.click() в дебаге все работает
         browserSetup(browser)
         searchPage = SearchPage(driver)
         searchPage.searchField.sendKeys("хостинг")
         searchPage.searchField.sendKeys(Keys.ENTER)
-        val wait = WebDriverWait(driver, Duration.ofSeconds(10)) // Ждем до 10 секунд
+        sleep(1000)
+        val wait = WebDriverWait(driver, Duration.ofSeconds(10))
         val element =
             wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"__layout\"]/div/div[3]/div[2]/div/div[2]/div[1]/div[1]/div[1]")))
         element.click()
 
         sleep(5000)
 
-        assert(driver.currentUrl == "https://timeweb.com/ru/docs/virtualnyj-hosting")
+        assertEquals(driver.currentUrl, "https://timeweb.com/ru/docs/virtualnyj-hosting")
     }
 }
